57b28a157bfaaa8b9a67962be12b050a81aee599
vafanobakht23 <vafa.n@greenhabitat.at>
2023-08-02 20:15:36 +0330

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Register from "./pages/Register";
 import React from "react";
 import "./App.generated.css";
 import Login from "./pages/Login";
@@ -6,6 +5,12 @@ import Login from "./pages/Login";
 function App(): React.ReactElement {
   return (
     <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
       <Login />
       {/* <Register /> */}
     </>
