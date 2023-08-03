ad7021426ba0c857e6b5f776e95738027ba712d2
vafanobakht23 <vafa.n@greenhabitat.at>
2023-08-03 13:26:51 +0330

    fix: change from for register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,107 +1,91 @@
-import { Button, Checkbox, Input, Select } from "antd";
-import { Form, Formik } from "formik";
-import { Link, useNavigate } from "react-router-dom";
-import * as Yup from "yup";
+import { Button, Checkbox, Form, Input, Select } from "antd";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const validationSchema = Yup.object().shape({
-    firstname: Yup.string().required("Firstname is required"),
-    lastname: Yup.string().required("Lastname is required"),
-    email: Yup.string().email("Invalid email").required("Email is required"),
-    password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
-      .required("Password is required"),
-  });
   const options = [
     { value: "Intern", label: "Intern" },
     { value: "Company", label: "Company" },
   ];
-  const navigate = useNavigate();
-
+  const onFinish = (formData: any) => {
+    // Send the form data to the server
+    console.log(JSON.stringify(formData));
+  };
   return (
     <div className="">
       <div className="flex flex-col items-center justify-center h-screen border-green-700 rounded-2xl">
-        <Formik
-          initialValues={{
-            firstname: "",
-            lastname: "",
-            email: "",
-            password: "",
-            agree: false,
-          }}
-          validationSchema={validationSchema}
-          onSubmit={(): void => console.log("vafa")}
+        <p className="text-green-800 text-xl mb-1">Sign up</p>
+        <Form
+          onFinish={onFinish}
+          className="w-1/3 mx-auto p-8 shadow-xl rounded-md border-l-green-700"
         >
-          <Form className="w-1/3 mx-auto p-8 shadow-xl rounded-md border-l-green-700">
-            <p className="text-green-800 text-xl mb-1">Sign up</p>
-            <div className="my-2 w-full">
-              <Input
-                placeholder="Firstname"
-                name="firstname"
-                id="firstnmae"
-                className="border-green-500 rounded-md"
-              ></Input>
-            </div>
-            <div className="my-2">
-              <Input
-                placeholder="Lastname"
-                name="lastname"
-                id="lastname"
-                className="border-green-500 rounded-md"
-              ></Input>
-            </div>
-            <div className="my-2">
-              <Input
-                placeholder="Email"
-                name="lastname"
-                id="lastname"
-                type="email"
-                className="border-green-500 rounded-md"
-              ></Input>
-            </div>
-            <div className="my-2">
-              <Input
-                placeholder="Password"
-                name="password"
-                id="password"
-                type="password"
-                className="border-green-500 rounded-md"
-              ></Input>
-            </div>
-            <div className="my-2 w-full flex">
-              <Select
-                className="w-full flex border-green-500 rounded-md"
-                defaultValue={options[0]}
-              >
-                {options.map((option) => (
-                  <option
-                    key={option.value}
-                    value={option.value}
-                    className="w-full"
-                  >
-                    {option.label}
-                  </option>
-                ))}
-              </Select>
-            </div>
-            <div className="my-1 flex gap-x-2">
-              <Checkbox />
-              <span>I agree with terms to service</span>
-            </div>
-            <div className="mt-3">
-              <Button className="bg-green-400 w-full">Create account</Button>
-            </div>
-            <div className="flex gap-x-1">
-              <p className="mt-1">Already member?</p>
-              <Link
-                to={"/"}
-                className="text-green-600 mt-1 underline cursor-pointer focus:text-green-600"
-              >
-                Sign in
-              </Link>
-            </div>
-          </Form>
-        </Formik>
+          <Form.Item className="my-2 w-full" name="firstname">
+            <Input
+              placeholder="Firstname"
+              id="firstnmae"
+              className="border-green-500 rounded-md"
+            />
+          </Form.Item>
+          <Form.Item className="my-2" name="lastname">
+            <Input
+              placeholder="Lastname"
+              id="lastname"
+              className="border-green-500 rounded-md"
+            />
+          </Form.Item>
+          <Form.Item className="my-2" name="email">
+            <Input
+              placeholder="Email"
+              id="lastname"
+              type="email"
+              className="border-green-500 rounded-md"
+            />
+          </Form.Item>
+          <Form.Item className="my-2" name="password">
+            <Input
+              placeholder="Password"
+              id="password"
+              type="password"
+              className="border-green-500 rounded-md"
+            />
+          </Form.Item>
+          <Form.Item className="my-2 w-full flex" name="role">
+            <Select
+              className="border-green-500 rounded-md"
+              defaultValue={options[0]}
+            >
+              {options.map((option) => (
+                <Select.Option
+                  key={option.value}
+                  value={option.value}
+                  className="w-full"
+                >
+                  {option.label}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            className="my-1 flex gap-x-2"
+            name="isSubscribed"
+            valuePropName="checked"
+          >
+            <Checkbox>I agree with terms to service</Checkbox>
+          </Form.Item>
+          <Form.Item className="mt-3">
+            <Button htmlType="submit" className="bg-green-400 w-full">
+              Create account
+            </Button>
+          </Form.Item>
+          <div className="flex gap-x-1">
+            <p className="mt-1">Already member?</p>
+            <Link
+              to={"/"}
+              className="text-green-600 mt-1 underline cursor-pointer focus:text-green-600"
+            >
+              Sign in
+            </Link>
+          </div>
+        </Form>
       </div>
     </div>
   );
