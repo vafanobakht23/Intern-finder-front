5d2215c4c468b07bc3a9865c2eb97baeeea4363a
vafanobakht23 <vafa.n@greenhabitat.at>
2023-08-06 19:37:19 +0330

    feat: add use api for fetching data

--- src/api/useApi.ts
+++ src/api/useApi.ts
@@ -1,2 +1,37 @@
-const useApi = () => {};
-export default useApi;
+import { useState, useEffect } from "react";
+
+export type TApiResponse = {
+  status: Number;
+  statusText: String;
+  data: any;
+  error: any;
+  loading: Boolean;
+};
+
+export const useApi = (url: string): TApiResponse => {
+  const [status, setStatus] = useState<Number>(0);
+  const [statusText, setStatusText] = useState<String>("");
+  const [data, setData] = useState<any>();
+  const [error, setError] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const getAPIData = async () => {
+    setLoading(true);
+    try {
+      const apiResponse = await fetch(url);
+      const json = await apiResponse.json();
+      setStatus(apiResponse.status);
+      setStatusText(apiResponse.statusText);
+      setData(json);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getAPIData();
+  }, []);
+
+  return { status, statusText, data, error, loading };
+};
